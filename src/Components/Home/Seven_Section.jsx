ba859246de5c5e9a206fb3cd9c29752c86e42a0b
Aaron <support@mitash.com>
2024-11-28 17:53:23 +0530

    Latest Changes

--- src/Components/Home/Seven_Section.jsx
+++ src/Components/Home/Seven_Section.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
  
 import '../../Css/Home/Seven.css'
+import i18n from '../../i18n'
+import { useTranslation } from 'react-i18next'
 
 const Seven_Section = () => {
+  const{t,i18n} =useTranslation();
   return (
     <>
       <div className='main-seven'>
@@ -10,10 +13,9 @@ const Seven_Section = () => {
 
           <div className="right-seven-sec-item">
             <h1> 
-            Discover Local Businesses That Accept CASHLESSPAY
- 
+            {t("seven-line1")}
              </h1>
-            <p> Leave cash behind. Explore local businesses that accept CASHLESSPAY and enjoy exclusive discounts for travelers. Starting in SEA and ANZ regions, with plans to expand to other popular destinations soon.</p>
+            <p> {t("seven-line2")}</p>
           </div>
 
           <div className="left-seven-sec-item">
