9b7ea2cec9e1df4d24eb5ad889be45a1022142b0
Aaron <support@mitash.com>
2024-12-03 16:42:34 +0530

    Latest update

--- api/send-email.js
+++ api/send-email.js
@@ -1,5 +1,8 @@
-import nodemailer from 'nodemailer';
-
 export default async function handler(req, res) {
-    console.log("test")
-}
+    if (req.method === 'POST') {
+      // Example alert message
+      return res.status(200).json({ message: 'Alert triggered successfully!' });
+    } else {
+      return res.status(405).json({ error: 'Method not allowed' });
+    }
+  }  
