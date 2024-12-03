1fb1b836d75dcdc3887cb7838528a71178b2d898
Aaron <support@mitash.com>
2024-12-03 17:04:33 +0530

    Latest update

--- api/send-email.js
+++ api/send-email.js
@@ -11,8 +11,6 @@ export default async function handler(req, res) {
     return res.status(400).json({ error: 'Missing required fields' });
   }
 
-  return res.status(400).json({ error: process.env.EMAIL_PASS});
-
   try {
     const transporter = nodemailer.createTransport({
       service: 'Gmail',
