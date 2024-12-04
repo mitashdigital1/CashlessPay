f79e68a663d57c6b7bf7c6134611a0f3d4af6ac7
Aaron <support@mitash.com>
2024-12-04 16:12:50 +0530

    Latest changes

--- api/send-email.js
+++ api/send-email.js
@@ -24,7 +24,7 @@ export default async function handler(req, res) {
   try {
     const transporter = nodemailer.createTransport({
       host: 'smtp.zoho.com',
-      port: 465, // Zoho SMTP uses port 465 for SSL
+      port: 587, // Zoho SMTP uses port 465 for SSL
       secure: true, // True for SSL
       auth: {
         user: process.env.EMAIL_USER, // Your Zoho email address
