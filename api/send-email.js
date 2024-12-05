f883a3c1c583f55b2150b371c00a5b0f5ae082d9
Aaron <support@mitash.com>
2024-12-05 14:22:18 +0530

    Latest Changes

--- api/send-email.js
+++ api/send-email.js
@@ -33,7 +33,7 @@ export default async function handler(req, res) {
 
     const mailOptions = {
       from: process.env.EMAIL_USER,
-      to:email,
+      to:'cashpnoreply@gmail.com',
       subject:reason,
       text: messageData,
     };
